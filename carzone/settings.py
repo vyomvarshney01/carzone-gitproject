0c0a497f1718f5880e0a56bf1203128ab044d1d6
vyomvarshney01 <vyomvarshney400@gmail.com>
2022-07-11 17:37:50 +0530

    change whitenoise middleware

--- carzone/settings.py
+++ carzone/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'carzone.urls'
